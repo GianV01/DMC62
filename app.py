212fa8aa3b5ac2fbe1b847047c89e5827153ae1b
GianV01 <giancarlovaldivia2001@gmail.com>
2026-09-03 22:16:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,15 @@ st.sidebar.title("Parametros de Ingreso")
 st.write("Elaborado por: Giancarlo Valdivia")
 
 modulos = st.sidebar.selectbox ("Seleccione el módulo",["Listas","Arreglos","Funciones","POO"])
+
+if modulos == "Listas":
+  st.write("Te encuentras en el modulo de listas")
+
+elif modulos == "Arreglos":
+  st.write("Te encuentras en el modulo arreglos")
+
+elif modulos == "Funciones":
+  st.write("Te encuentras en el modulo Funciones")
+
+else:
+  st.write("No te encuentrsa en ningun codigo")
