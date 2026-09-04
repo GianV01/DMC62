19ddcd7b793bb2da001a58e66c1151c178dc1b8e
GianV01 <giancarlovaldivia2001@gmail.com>
2026-09-03 22:12:45 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ st.title ("Especialización Pyhton for Analitycs")
 st.sidebar.title("Parametros de Ingreso")
 st.write("Elaborado por: Giancarlo Valdivia")
 
-modulos = st.selectbox ("Seleccione el módulo",["Listas","Arreglos","Funciones","POO"])
+modulos = st.sidebar.selectbox ("Seleccione el módulo",["Listas","Arreglos","Funciones","POO"])
