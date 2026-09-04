d7a6f08c126bbb86302b2394c76a4b06602c33db
GianV01 <giancarlovaldivia2001@gmail.com>
2026-09-03 22:11:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,5 @@ import streamlit as st
 st.title ("Especialización Pyhton for Analitycs")
 st.sidebar.title("Parametros de Ingreso")
 st.write("Elaborado por: Giancarlo Valdivia")
+
+modulos = st.selectbox ("Seleccione el módulo",["Listas","Arreglos","Funciones","POO"])
