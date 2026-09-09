879ca41e96505ab38e04b57090f0009c2892101d
GianV01 <giancarlovaldivia2001@gmail.com>
2026-09-08 22:20:50 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,8 @@ st.title ("Especialización Pyhton for Analitycs")
 st.sidebar.title("Parametros de Ingreso")
 st.write("Elaborado por: Giancarlo Valdivia")
 
-st.image("python_logo.png")
+st.image("python_logo.png", width =300)
+st.sidebar.image("DMC.png")
 modulos = st.sidebar.selectbox ("Seleccione el módulo",["Listas","Arreglos","Funciones","POO"])
 
 if modulos == "Listas":
