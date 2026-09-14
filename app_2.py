58a0787f4191a1a6e92864d9419e9350af8c3749
GianV01 <giancarlovaldivia2001@gmail.com>
2026-09-13 22:33:26 -0500

    Update app_2.py

--- app_2.py
+++ app_2.py
@@ -1,4 +1,6 @@
 import streamlit as st
 
+st.sidebar.title("Secciones")
+st.divider ()
 Secciones = st.sidebar.selectbox("Selecione el módulo",["Home", "Ejercicio 1", "Ejercicio 2", "Ejercicio 3","Ejercicio 4"])
 
