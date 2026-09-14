762b79baa6521a18399541e9c9fa5f380ae98b9a
GianV01 <giancarlovaldivia2001@gmail.com>
2026-09-13 22:59:59 -0500

    Update app_2.py

--- app_2.py
+++ app_2.py
@@ -9,7 +9,7 @@ if secciones == "Home":
 col1, col3 = st.columns(2)
 
 with col1:
-  st.image("DMC.png", width="stretch")
+  st.image("DMC.png", width="100")
 
 with col3:
   st.image("python_logo.png",width="stretch")
