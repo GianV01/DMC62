5ec3835cc22d526b4dd0a096f39b709942e8029e
GianV01 <giancarlovaldivia2001@gmail.com>
2026-09-14 01:17:26 -0500

    Update app_2.py

--- app_2.py
+++ app_2.py
@@ -4,7 +4,7 @@ st.sidebar.title("Secciones")
 secciones = st.sidebar.selectbox("Selecione el módulo", ["Home", "Ejercicio 1", "Ejercicio 2", "Ejercicio 3", "Ejercicio 4"])
 
 if secciones == "Home":
-    st.markdown("<h1 style='text-align: center;'>PROYECTO 1 – APLICACIÓN EN STREAMLIT</h1>", unsafe_allow_html=True)
+    st.title("PROYECTO 1 – APLICACIÓN EN STREAMLIT")
     st.divider()
 
     col1, col2 = st.columns(2)
@@ -17,45 +17,45 @@ if secciones == "Home":
 
     st.divider()
 
-    st.markdown("<h3 style='text-align: center;'>Módulo 1 – Python Fundamentals</h3>", unsafe_allow_html=True)
+    st.subheader(" Módulo 1 – Python Fundamentals ")
     st.divider()
 
-    st.markdown("<p style='text-align: center;'>Se va a desarrollar una aplicación interactiva haciendo uso de las plataformas GitHub y Streamlit, integrando los contenidos revisados en el módulo.</p>", unsafe_allow_html=True)
+    st.markdown("Se va a desarrollar una aplicación interactiva haciendo uso de las plataformas GitHub y Streamlit, integrando los contenidos revisados en el módulo.")
     st.divider()
 
-    st.markdown("<h3 style='text-align: center;'>Tecnologias Utilizadas</h3>", unsafe_allow_html=True)
+    st.subheader(" Tecnologias Utilizadas ")
 
     col3, col4, col5, col6 = st.columns(4)
 
     with col3:
         with st.container(border=True):
-            st.markdown("<p style='text-align: center;'><b>Python</b></p>", unsafe_allow_html=True)
+            st.markdown(" **Python** ")
 
     with col4:
         with st.container(border=True):
-            st.markdown("<p style='text-align: center;'><b>GitHub</b></p>", unsafe_allow_html=True)
+            st.markdown(" **GitHub** ")
 
     with col5:
         with st.container(border=True):
-            st.markdown("<p style='text-align: center;'><b>Streamlit</b></p>", unsafe_allow_html=True)
+            st.markdown(" **Streamlit** ")
 
     with col6:
         with st.container(border=True):
-            st.markdown("<p style='text-align: center;'><b>Librerias</b></p>", unsafe_allow_html=True)
+            st.markdown(" **Librerias** ")
 
     st.divider()
 
-    st.markdown("<h3 style='text-align: center;'>Elaborado por Giancarlo Esteban Valdivia Asencio</h3>", unsafe_allow_html=True)
+    st.subheader(" Elaborado por Giancarlo Esteban Valdivia Asencio")
     st.divider()
 
-    st.markdown("<p style='text-align: justify;'>Bachiller en la carrera de Ingenieria de Sistemas e Informatica, egresado de la universidad Tecnologica del Perú en el año 2025 cuento con 4 años de experiencia laboral entre practicas pre-profesionales, practicas profesionales y puestos laborales directos, actualmente me encuentro laborando en la empresa Molitalia, y mi interesa seguir formandome en la administracion de data.</p>", unsafe_allow_html=True)
+    st.markdown("Bachiller en la carrera de Ingenieria de Sistemas e Informatica, egresado de la universidad Tecnologica del Perú en el año 2025 cuento con 4 años de experiencia laboral entre practicas pre-profesionales, practicas profesionales y puestos laborales directos, actualmente me encuentro laborando en la empresa Molitalia, y mi interesa seguir formandome en la administracion de data.")
     st.divider()
 
-    st.markdown("<h3 style='text-align: center;'>2026</h3>", unsafe_allow_html=True)
+    st.subheader(" 2026 ")
 
+# La única modificación crítica fue alinearlo al margen izquierdo (mismo nivel que 'if')
 elif secciones == "Ejercicio 1":
     st.write("Te encuentras en el ejercicio 1")
-
 
 
 
