9a24e0845524d9f6c6c0b61177036e363ccdfe14
GianV01 <giancarlovaldivia2001@gmail.com>
2026-09-14 00:14:48 -0500

    Update app_2.py

--- app_2.py
+++ app_2.py
@@ -22,8 +22,8 @@ st.subheader (" Módulo 1 – Python Fundamentals ", text_alignment="center")
 
 st.divider()
 
-texto1 = ""Se va a desarrollar una aplicación interactiva dentro haciendo uso de las plataformas GitHub y Streamlit,  
-integrando los contenidos revisados en el módulo.""  
+texto1 = "" Se va a desarrollar una aplicación interactiva dentro haciendo uso de las plataformas GitHub y Streamlit,  
+integrando los contenidos revisados en el módulo.""
 
 st.markdown(texto1, text_alignment="justify")
 
