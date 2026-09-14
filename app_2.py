86e8be20e90dbd54a7b1f072dcbceaddd0601e01
GianV01 <giancarlovaldivia2001@gmail.com>
2026-09-14 00:38:54 -0500

    Update app_2.py

--- app_2.py
+++ app_2.py
@@ -38,9 +38,9 @@ st.subheader (" 2026 ", text_alignment="center")
 
 st.divider()
 
-col3, col4, col5, col6 = st.columns(4)
-
 st.subheader (" Tecnologias Utilizadas ", text_alignment="center")
+
+col3, col4, col5, col6 = st.columns(4)
 
 with col3:
   with st.container(border=True):
