1d65611471ce0b6f72c35f75451c65b7146785b2
GianV01 <giancarlovaldivia2001@gmail.com>
2026-09-13 23:07:27 -0500

    Update app_2.py

--- app_2.py
+++ app_2.py
@@ -9,7 +9,8 @@ if secciones == "Home":
 col1, col3 = st.columns(2)
 
 with col1:
-  st.image("DMC.png", width=150)
+  with st.container(horizontal_alignment="center"):
+    st.image("DMC.png", width=150)
 
 with col3:
   st.image("python_logo.png",width="stretch")
