af609423b04b8937c1b73eaf0fa2a027a36b12fc
GianV01 <giancarlovaldivia2001@gmail.com>
2026-09-14 01:18:49 -0500

    Update app_2.py

--- app_2.py
+++ app_2.py
@@ -4,58 +4,74 @@ st.sidebar.title("Secciones")
 secciones = st.sidebar.selectbox("Selecione el módulo", ["Home", "Ejercicio 1", "Ejercicio 2", "Ejercicio 3", "Ejercicio 4"])
 
 if secciones == "Home":
-    st.title("PROYECTO 1 – APLICACIÓN EN STREAMLIT")
-    st.divider()
+  st.title("PROYECTO 1 – APLICACIÓN EN STREAMLIT",text_alignment="center")
+  st.divider()
+ 
+col1, col2 = st.columns(2)
 
-    col1, col2 = st.columns(2)
+with col1:
+  with st.container(horizontal_alignment="center"):
+    st.image("DMC.png", width=150)
 
-    with col1:
-        st.image("DMC.png", width=150)
+with col2:
+  with st.container(horizontal_alignment="center"):
+    st.image("python_logo.png",width=200)
 
-    with col2:
-        st.image("python_logo.png", width=200)
+st.divider()
+                        
+st.subheader (" Módulo 1 – Python Fundamentals ", text_alignment="center")
 
-    st.divider()
+st.divider()
 
-    st.subheader(" Módulo 1 – Python Fundamentals ")
-    st.divider()
+st.markdown("Se va a desarrollar una aplicación interactiva haciendo uso de las plataformas GitHub y Streamlit,integrando los contenidos revisados en el módulo.", text_alignment="justify")
 
-    st.markdown("Se va a desarrollar una aplicación interactiva haciendo uso de las plataformas GitHub y Streamlit, integrando los contenidos revisados en el módulo.")
-    st.divider()
+st.divider()
 
-    st.subheader(" Tecnologias Utilizadas ")
+st.subheader (" Tecnologias Utilizadas ", text_alignment="center")
 
-    col3, col4, col5, col6 = st.columns(4)
+col3, col4, col5, col6 = st.columns(4)
 
-    with col3:
-        with st.container(border=True):
-            st.markdown(" **Python** ")
+with col3:
+  with st.container(border=True):
+    st.markdown(" **Python** ",text_alignment="center")
 
-    with col4:
-        with st.container(border=True):
-            st.markdown(" **GitHub** ")
+with col4:
+  with st.container(border=True):
+    st.markdown(" **GitHub** ",text_alignment="center")
 
-    with col5:
-        with st.container(border=True):
-            st.markdown(" **Streamlit** ")
+with col5:
+  with st.container(border=True):
+    st.markdown(" **Streamlib** ",text_alignment="center")
 
-    with col6:
-        with st.container(border=True):
-            st.markdown(" **Librerias** ")
+with col6:
+  with st.container(border=True):
+    st.markdown(" **Librerias** ",text_alignment="center")
 
-    st.divider()
+st.divider()
 
-    st.subheader(" Elaborado por Giancarlo Esteban Valdivia Asencio")
-    st.divider()
+st.subheader (" Elaborado por Giancarlo Esteban Valdivia Asencio", text_alignment="center")
 
-    st.markdown("Bachiller en la carrera de Ingenieria de Sistemas e Informatica, egresado de la universidad Tecnologica del Perú en el año 2025 cuento con 4 años de experiencia laboral entre practicas pre-profesionales, practicas profesionales y puestos laborales directos, actualmente me encuentro laborando en la empresa Molitalia, y mi interesa seguir formandome en la administracion de data.")
-    st.divider()
+st.divider()
 
-    st.subheader(" 2026 ")
+st.markdown ("Bachiller en la carrera de Ingenieria de Sistemas e Informatica, egresado de la universidad Tecnologica del Perú en el año 2025 cuento con 4 años de experiencia laboral entre practicas pre-profesionales, practicas profesionales y puestos laborales directos,actualmente me encuentro laborando en la empresa Molitalia, y mi interesa seguir formandome en la administracion de data. ", text_alignment= "justify")
 
-# La única modificación crítica fue alinearlo al margen izquierdo (mismo nivel que 'if')
+st.divider()
+
+st.subheader (" 2026 ", text_alignment="center")
+
+st.divider()
 elif secciones == "Ejercicio 1":
-    st.write("Te encuentras en el ejercicio 1")
+  st.write ("Te encuentras en el Ejercicio 1")
+  
+elif secciones == "Ejercicio 2":
+  st.write ("Te encuentras en el Ejercicio 2")
+  
+elif secciones == "Ejercicio 3":
+  st.write ("Te encuentras en el Ejercicio 3")
+  
+else:
+  st.write ("Te encuentras en el Ejercicio 4")
+
 
 
 
