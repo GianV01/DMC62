4a6cc78886c24d189304b9237b1fef0d6e4b2340
GianV01 <giancarlovaldivia2001@gmail.com>
2026-09-13 23:59:44 -0500

    Update app_2.py

--- app_2.py
+++ app_2.py
@@ -26,8 +26,7 @@ st.subheader (" Elaborado por Giancarlo Esteban Valdivia Asencio", text_alignmen
 
 st.divider()
 
-st.markdown ("Bachiller en la carrera de Ingenieria de Sistemas e Informatica, egresado de la universidad Tecnologica del Perú en el año 2025 cuento con 4 años de experiencia laboral entre practicas pre-profesionales, practicas profesionales y puestos laborales directos,
-actualmente me encuentro laborando en la empresa Molitalia, y mi interesa seguir formandome en la administracion de data", text_alignment= "justify")
+st.markdown ("Bachiller en la carrera de Ingenieria de Sistemas e Informatica, egresado de la universidad Tecnologica del Perú en el año 2025 cuento con 4 años de experiencia laboral entre practicas pre-profesionales, practicas profesionales y puestos laborales directos,actualmente me encuentro laborando en la empresa Molitalia, y mi interesa seguir formandome en la administracion de data ", text_alignment= "justify")
 
 st.divider()
 
