84e66f0cda4b6707d4cedf74f84ee181602bc576
GianV01 <giancarlovaldivia2001@gmail.com>
2026-09-13 22:49:50 -0500

    Update app_2.py

--- app_2.py
+++ app_2.py
@@ -5,4 +5,15 @@ secciones = st.sidebar.selectbox("Selecione el módulo",["Home", "Ejercicio 1", 
 
 if secciones == "Home":
   st.title("PROYECTO 1 – APLICACIÓN EN STREAMLIT")
-  st.header("PROYECTO 1 – APLICACIÓN EN STREAMLIT")
+ 
+col1, col2, col3 = st.columns(3)
+
+with col1:
+  st.image("DMC.png")
+
+with col2:
+    
+    st.image("")
+
+with col3:
+  st.image("python_logo.png")
