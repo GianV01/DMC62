673e26d8ca5972b9c06e6a21209b0ccdc14374d4
GianV01 <giancarlovaldivia2001@gmail.com>
2026-09-14 00:35:35 -0500

    Update app_2.py

--- app_2.py
+++ app_2.py
@@ -41,8 +41,8 @@ st.divider()
 col3, col4, col5, col6 = st.columns(4)
 
 with col3:
-  with st.container(horizontal_alignment="center", border=True):
-    st.markdown(" **Python** ")
+  with st.container(border=True):
+    st.markdown(" **Python** ",text_alignment="left")
 
 with col4:
   with st.container(horizontal_alignment="right"):
