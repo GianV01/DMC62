c47629e4fabd4712b05375039d1563a66cb8066b
GianV01 <giancarlovaldivia2001@gmail.com>
2026-09-14 01:34:02 -0500

    Update app_2.py

--- app_2.py
+++ app_2.py
@@ -4,7 +4,7 @@ st.sidebar.title("Secciones")
 secciones = st.sidebar.selectbox("Selecione el módulo", ["Home", "Ejercicio 1", "Ejercicio 2", "Ejercicio 3", "Ejercicio 4"])
 
 if secciones == "Home":
-    st.title("PROYECTO 1 – APLICACIÓN EN STREAMLIT")
+    st.title("PROYECTO 1 – APLICACIÓN EN STREAMLIT",text_alignment="center")
     st.divider()
  
     col1, col2 = st.columns(2)
@@ -17,41 +17,41 @@ if secciones == "Home":
 
     st.divider()
                         
-    st.subheader(" Módulo 1 – Python Fundamentals ")
+    st.subheader(" Módulo 1 – Python Fundamentals ",text_alignment="center")
     st.divider()
 
     st.markdown("Se va a desarrollar una aplicación interactiva haciendo uso de las plataformas GitHub y Streamlit, integrando los contenidos revisados en el módulo.")
     st.divider()
 
-    st.subheader(" Tecnologias Utilizadas ")
+    st.subheader(" Tecnologias Utilizadas ",text_alignment="center")
 
     col3, col4, col5, col6 = st.columns(4)
 
     with col3:
         with st.container(border=True):
-            st.markdown(" **Python** ")
+            st.markdown(" **Python** ",text_alignment="center")
 
     with col4:
         with st.container(border=True):
-            st.markdown(" **GitHub** ")
+            st.markdown(" **GitHub** ",text_alignment="center")
 
     with col5:
         with st.container(border=True):
-            st.markdown(" **Streamlit** ")
+            st.markdown(" **Streamlit** ",text_alignment="center")
 
     with col6:
         with st.container(border=True):
-            st.markdown(" **Librerias** ")
+            st.markdown(" **Librerias** ",text_alignment="center")
 
     st.divider()
 
     st.subheader(" Elaborado por Giancarlo Esteban Valdivia Asencio")
     st.divider()
 
-    st.markdown("Bachiller en la carrera de Ingenieria de Sistemas e Informatica, egresado de la universidad Tecnologica del Perú en el año 2025 cuento con 4 años de experiencia laboral entre practicas pre-profesionales, practicas profesionales y puestos laborales directos, actualmente me encuentro laborando en la empresa Molitalia, y mi interesa seguir formandome en la administracion de data.")
+    st.markdown("Bachiller en la carrera de Ingenieria de Sistemas e Informatica, egresado de la universidad Tecnologica del Perú en el año 2025 cuento con 4 años de experiencia laboral entre practicas pre-profesionales, practicas profesionales y puestos laborales directos, actualmente me encuentro laborando en la empresa Molitalia, y mi interesa seguir formandome en la administracion de data.",text_alignment="justify")
     st.divider()
 
-    st.subheader(" 2026 ")
+    st.subheader(" 2026 ",text_alignment="centre")
 
 elif secciones == "Ejercicio 1":
   st.write ("Te encuentras en home")
