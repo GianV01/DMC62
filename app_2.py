3171442536a18f9f2d8752a0d63be6947d93cf32
GianV01 <giancarlovaldivia2001@gmail.com>
2026-09-13 23:27:10 -0500

    Update app_2.py

--- app_2.py
+++ app_2.py
@@ -19,6 +19,7 @@ with col3:
 st.divider()
 
 st.write("Elaborado por **Giancarlo Esteban Valdivia Asencio**")
+st.caption("Elaborado por **Giancarlo Esteban Valdivia Asencio**")
                          
 st.divider()
 
