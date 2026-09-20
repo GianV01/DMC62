e49e70d825afa5f1770f7b85964c6824c4b97c0d
GianV01 <giancarlovaldivia2001@gmail.com>
2026-09-20 00:23:18 -0500

    Fix formatting in app_2.py

--- app_2.py
+++ app_2.py
@@ -59,7 +59,7 @@ if secciones == "Home":
     st.subheader(" 2026 ",text_alignment="center")
 
 elif secciones == "Ejercicio 1":
-        
+
 elif secciones == "Ejercicio 2":
     st.title("FORMULARIO DE REGISTRO", text_alignment="center")
     st.divider()
