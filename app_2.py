20391fe98cd5da30d59283e962dc489d7e178ee8
GianV01 <giancarlovaldivia2001@gmail.com>
2026-09-20 00:53:36 -0500

    Add calculations for total ingresos, gastos, and saldo final
    Calculate total ingresos and gastos, and compute saldo final.

--- app_2.py
+++ app_2.py
@@ -75,6 +75,22 @@ elif secciones == "Ejercicio 1":
     if st.button("Agregar Movimiento"):
         if concepto.strip() != "" and valor > 0:
             st.session_state.vomimiento.append({"Concepto": concepto, "Tipo": tipo, "Valor": valor})
+
+    if st.session_state.movimientos:
+        st.dataframe(st.session_state.movimientos)
+
+        
+        total_ingresos = sum(
+            m["valor"]
+            for m in st.session_state.movimientos
+            if m["tipo"] == "Ingreso"
+        )
+        total_gastos = sum(
+            m["valor"]
+            for m in st.session_state.movimientos
+            if m["tipo"] == "Gasto"
+        )
+        saldo_final = total_ingresos - total_gastos
         
     
 
