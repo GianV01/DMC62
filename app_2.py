e1d765caef36db44f1b496b14da3af060eacfae3
GianV01 <giancarlovaldivia2001@gmail.com>
2026-09-20 01:04:28 -0500

    Update app_2.py

--- app_2.py
+++ app_2.py
@@ -74,7 +74,7 @@ elif secciones == "Ejercicio 1":
 
     if st.button("Agregar Movimiento"):
         if concepto.strip() != "" and valor > 0:
-            st.session_state.movimiento.append({"concepto": concepto, "tipo": tipo, "valor": valor})
+            st.session_state.movimientos.append({"concepto": concepto, "tipo": tipo, "valor": valor})
             
             if st.session_state.movimientos:
                 st.dataframe(st.session_state.movimientos)
