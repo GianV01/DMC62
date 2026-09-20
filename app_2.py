3f6f430d41b35bfd433ca621139acc698a5171d8
GianV01 <giancarlovaldivia2001@gmail.com>
2026-09-20 03:47:56 -0500

    Update app_2.py

--- app_2.py
+++ app_2.py
@@ -284,25 +284,25 @@ elif secciones == "Ejercicio 3":
     
     if btn_ejecutar:
         try:
-                resultado = calcular_almacenamiento_respaldo(
-                numero_usuarios=int(numero_usuarios),
-                archivos_por_usuario=int(archivos_por_usuario),
-                tamano_promedio_mb=float(tamano_promedio_mb),
-                factor_respaldo=float(factor_respaldo)
-                )
-                st.write(f"✅ **Resultado para '{etiqueta}' ({funcion_seleccionada}):**")
-                st.write(f"- **Almacenamiento Necesario (MB):** {resultado['almacenamiento_estimado_mb']:,} MB")
-                st.write(f"- **Almacenamiento Necesario (GB):** {resultado['almacenamiento_estimado_gb']:,} GB")
+            resultado = calcular_almacenamiento_respaldo(
+            numero_usuarios=int(numero_usuarios),
+            archivos_por_usuario=int(archivos_por_usuario),
+            tamano_promedio_mb=float(tamano_promedio_mb),
+            factor_respaldo=float(factor_respaldo))
+
+            st.write(f"✅ **Resultado para '{etiqueta}' ({funcion_seleccionada}):**")
+            st.write(f"- **Almacenamiento Necesario (MB):** {resultado['almacenamiento_estimado_mb']:,} MB")
+            st.write(f"- **Almacenamiento Necesario (GB):** {resultado['almacenamiento_estimado_gb']:,} GB")
             
-        nuevo_registro = pd.DataFrame([{
-            "Etiqueta": etiqueta,
-            "Tipo Respaldo": funcion_seleccionada,
-            "N° Usuarios": numero_usuarios,
-            "Archivos/Usuario": archivos_por_usuario,
-            "Tamaño (MB)": tamano_promedio_mb,
-            "Factor": factor_respaldo,
-            "Estimado (MB)": resultado["almacenamiento_estimado_mb"],
-            "Estimado (GB)": resultado["almacenamiento_estimado_gb"]}])
+            nuevo_registro = pd.DataFrame([{
+                "Etiqueta": etiqueta,
+                "Tipo Respaldo": funcion_seleccionada,
+                "N° Usuarios": numero_usuarios,
+                "Archivos/Usuario": archivos_por_usuario,
+                "Tamaño (MB)": tamano_promedio_mb,
+                "Factor": factor_respaldo,
+                "Estimado (MB)": resultado["almacenamiento_estimado_mb"],
+                "Estimado (GB)": resultado["almacenamiento_estimado_gb"]}])
         
         st.session_state.historial_respaldos = pd.concat(
             [st.session_state.historial_respaldos, nuevo_registro], 
