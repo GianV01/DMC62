9dbe69b72599dfd5f03a0384df42074e1dc68e33
GianV01 <giancarlovaldivia2001@gmail.com>
2026-09-19 23:56:54 -0500

    Update app_2.py

--- app_2.py
+++ app_2.py
@@ -64,12 +64,7 @@ elif secciones == "Ejercicio 1":
     st.divider()
 
     def mostrar_ejercicio_1():
-    # Descripción del ejercicio
-    st.markdown("### Ejercicio 1 – Flujo de caja con listas")
-    st.markdown(
-        "Módulo para registrar movimientos financieros y calcular el saldo del flujo de caja."
-    )
-
+    
     # Inicialización de la lista vacía en el estado de la sesión
     if "movimientos" not in st.session_state:
         st.session_state.movimientos = []
