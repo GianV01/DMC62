e6a1d88e197139a14e8be1983a557f7cebfe2344
GianV01 <giancarlovaldivia2001@gmail.com>
2026-09-20 04:28:21 -0500

    Update app_2.py

--- app_2.py
+++ app_2.py
@@ -198,7 +198,7 @@ elif secciones == "Ejercicio 3":
     st.title(" Ejercicio 3 - Cálculo de Almacenamiento de Respaldo", text_alignment="center")
     st.divider()
     st.markdown("Esta herramienta permite calcular el **almacenamiento estimado necesario para respaldos**"  
-                " en función de la cantidad de usuarios, archivos y un factor de duplicación.")
+                " en función de la cantidad de usuarios, y la cantidad de archivos a respaldar.")
     st.divider()
 
     col1, col2 = st.columns(2)
