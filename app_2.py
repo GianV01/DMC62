d41cca0eca2e1795f67a10beb37ba8ed5a2daaaa
GianV01 <giancarlovaldivia2001@gmail.com>
2026-09-20 00:38:56 -0500

    Fix indentation for input fields in app_2.py

--- app_2.py
+++ app_2.py
@@ -68,9 +68,9 @@ elif secciones == "Ejercicio 1":
         if"movimientos" not in st.session_state:
             st.session_state.movimientos = []
 
-        concepto = st.text_input("Concepto:")
-        tipo = st.selectbox("Tipo de Movimiento:" , ["Ingreso","Gasto"])
-        valor = st.number_input("Valor:", min_value=0.0,format="%.2f")
+    concepto = st.text_input("Concepto:")
+    tipo = st.selectbox("Tipo de Movimiento:" , ["Ingreso","Gasto"])
+    valor = st.number_input("Valor:", min_value=0.0,format="%.2f")
     
 
     st.subheader(" Elaborado por Giancarlo Valdivia ",text_alignment="center")
