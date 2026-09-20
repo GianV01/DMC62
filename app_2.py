875c25732dc46369b533f24b02467284181832dd
GianV01 <giancarlovaldivia2001@gmail.com>
2026-09-20 04:27:20 -0500

    Update app_2.py

--- app_2.py
+++ app_2.py
@@ -212,7 +212,7 @@ elif secciones == "Ejercicio 3":
         
     btn_ejecutar = st.button("Ejecutar")
 
-    culculo_alm = lfp.calcular_almacenamiento_respaldo(numero_usuarios, archivos_por_usuario, tamano_promedio_mb, factor_respaldo)
+    calculo_alm = lfp.calcular_almacenamiento_respaldo(numero_usuarios,archivos_por_usuario,tamano_promedio_mb,factor_respaldo)
     st.write("El calculo de almacenamiento de respaldo es el siguiente:", calculo_alm)
 
 
