618fd6572e4a5513512dfda57cc585b998e54466
GianV01 <giancarlovaldivia2001@gmail.com>
2026-09-21 13:09:47 -0500

    Update title formatting in Home section

--- app.py
+++ app.py
@@ -160,7 +160,8 @@ secciones = st.sidebar.radio(
 
 if secciones == "🏠 Home":
     texto_centrado(" PORTAFORLIO | HERRAMIENTAS DE GESTIÓN CON PYTHON ", "h1", COLOR_TEXTO_PRINCIPAL, "2.5rem", bold=True)
-    st.title (" PORTAFORLIO ", text_alignment="center")
+    st.title (" | PORTAFORLIO | ", text_alignment="center")
+    st.title (" HERRAMIENTAS DE GESTIÓN CON PYTHON ", text_alignment="center")
     st.divider()
     
     
