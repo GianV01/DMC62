4c98839ed213d050fd9092f9f453b1e2849e090a
GianV01 <giancarlovaldivia2001@gmail.com>
2026-09-21 13:08:26 -0500

    Update app.py

--- app.py
+++ app.py
@@ -160,6 +160,7 @@ secciones = st.sidebar.radio(
 
 if secciones == "🏠 Home":
     texto_centrado(" PORTAFORLIO | HERRAMIENTAS DE GESTIÓN CON PYTHON ", "h1", COLOR_TEXTO_PRINCIPAL, "2.5rem", bold=True)
+    st.title (" PORTAFORLIO ", text_alignment="center")
     st.divider()
     
     
